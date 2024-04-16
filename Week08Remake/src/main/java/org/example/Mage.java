a0c2f2b1c388502abdda3a85039a22468aa2847d
Berktuğ <122936735+berktugarikan@users.noreply.github.com>
2024-04-16 16:06:50 +0300

    Add files via upload

---
+++ Week08Remake/src/main/java/org/example/Mage.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Mage extends Character {
+    public Mage(String name, int health, int attackPower) {
+        super(name, health, attackPower);
+    }
+
+    @Override
+    public void special_Attack(Character Opponent) {
+        Opponent.takeDamage(attackPower + 50);
+    }
+
+
+}
